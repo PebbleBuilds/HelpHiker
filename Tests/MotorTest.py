8c59bfea2bd20792fe5353909afe8097d1e51c52
kiribuilds@gmail.com <49044903+RoccoRuan@users.noreply.github.com>
2022-12-31 15:51:30 -0500

    Change the test

--- Tests/MotorTest.py
+++ Tests/MotorTest.py
@@ -14,10 +14,10 @@ def main():
     
     # creating a running loop
     while not rospy.is_shutdown():
-        motor_pub.publish(Vector3(speed, -speed, 0))
+        motor_pub.publish(Vector3(speed, speed, 0))
         waving_pub.publish(True)
         time.sleep(5)
-        motor_pub.publish(Vector3(-speed, speed, 0))
+        motor_pub.publish(Vector3(-speed, -speed, 0))
         waving_pub.publish(False)
         time.sleep(5)
 
