483983109570e7be800852a206885d38a5512451
kiribuilds@gmail.com <49044903+RoccoRuan@users.noreply.github.com>
2022-12-30 19:30:03 -0500

    added a motor test

---
+++ Tests/MotorTest.py
@@ -0,0 +1,28 @@
+import rospy
+from std_msgs.msg import Bool
+from geometry_msgs.msg import Vector3
+import time
+
+def main():
+    # define internal variables
+    speed = 255
+
+    # ROS stuff
+    motor_pub = rospy.Publisher('motors', Vector3, queue_size=10)
+    waving_pub = rospy.Publisher('driver_waving', Bool, queue_size=10)
+    rospy.init_node('keyboard_control', anonymous=True)
+    
+    # creating a running loop
+    while not rospy.is_shutdown():
+        motor_pub.publish(Vector3(255, -255, 0))
+        waving_pub.publish(True)
+        time.sleep(5)
+        motor_pub.publish(Vector3(0, 0, 0))
+        waving_pub.publish(False)
+        time.sleep(5)
+
+if __name__ == '__main__':
+    try:
+        main()
+    except rospy.ROSInterruptException:
+        print("ROS error.")
