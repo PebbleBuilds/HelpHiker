cb5c760955e983a3fe6b383a5b0028e00aca93a7
kiribuilds@gmail.com <49044903+RoccoRuan@users.noreply.github.com>
2023-01-14 18:52:28 -0500

    reverted to cv2 webserver, added debug

--- WebServer/WebServer.py
+++ WebServer/WebServer.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python
 from flask import Flask, render_template, Response
-import picamera
+#import picamera
 import cv2
 import socket
 import io
 
 app = Flask(__name__)
-vc = cv2.VideoCapture(0)
-#vc = cv2.VideoCapture('/dev/video0', cv2.CAP_V4L)
+vc = cv2.VideoCapture('/dev/video0', cv2.CAP_V4L)
 
 # set dimensions
 #vc.set(cv2.CAP_PROP_FRAME_WIDTH, 2560)
@@ -22,7 +21,11 @@ def index():
 def gen():
     """Video streaming generator function."""
     while True:
+        if not vc.isOpened():
+            print("vc not open")
         rval, frame = vc.read()
+        if not rval:
+            print("read failed")
         cv2.imwrite('t.jpg', frame)
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + open('t.jpg', 'rb').read() + b'\r\n')
