c0c8edb075622c397e0ff18dc3238c860bde8ed4
kiribuilds@gmail.com <49044903+RoccoRuan@users.noreply.github.com>
2023-01-14 19:15:25 -0500

    try to avoid using uv4l

--- WebServer/WebServer.py
+++ WebServer/WebServer.py
@@ -6,7 +6,7 @@ import socket
 import io
 
 app = Flask(__name__)
-vc = cv2.VideoCapture('/dev/video0', cv2.CAP_V4L)
+vc = cv2.VideoCapture(0)
 
 # set dimensions
 #vc.set(cv2.CAP_PROP_FRAME_WIDTH, 2560)
