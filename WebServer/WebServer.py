102e2ce9f7730da3fd4d68fb310be3e971c9acea
kiribuilds@gmail.com <49044903+RoccoRuan@users.noreply.github.com>
2023-01-14 19:19:48 -0500

    debug mode off

--- WebServer/WebServer.py
+++ WebServer/WebServer.py
@@ -39,5 +39,5 @@ def video_feed():
 
 
 if __name__ == '__main__':
-        app.run(host='0.0.0.0', debug=True, threaded=True)
+        app.run(host='0.0.0.0', debug=False, threaded=True)
                
