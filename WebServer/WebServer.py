cf9a900b092cca057b34faa3812e266d00469d90
PebbleBuilds <kiribuilds@gmail.com>
2023-02-21 16:41:37 -0500

    try 200

--- WebServer/WebServer.py
+++ WebServer/WebServer.py
@@ -9,8 +9,8 @@ import rospy
 from std_msgs.msg import Bool
 from geometry_msgs.msg import Vector3
 
-SPEED_1 = 50
-SPEED_2 = 50
+SPEED_1 = 200
+SPEED_2 = 200
 
 app = Flask(__name__)
 vc = cv2.VideoCapture(-1)
